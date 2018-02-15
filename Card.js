f0b6f14f8e87807fc9baf81d38e8c659011b6c9d
ilgoo jung <idjung4@gmail.com>
2018-02-15 00:02:47 -0500

    Card and Deck classes added
    Card
    - Standard rank and suit card
    
    Deck
    - Standard 52 deck of cards
    - Shuffle function
    - Draw function

---
+++ Card.js
@@ -0,0 +1,37 @@
+class Card {
+    constructor(rank, suit) {
+        let suits = { Clubs: 0, Diamonds: 1, Hearts: 2, Spades: 3 };
+        let ranks = { Two: 2, Three: 3, Four: 4, Five: 5, Six: 6, Seven: 7, Eight: 8, 
+                      Nine: 9, Ten: 10, Jack: 11, Queen: 12, King: 13, Ace: 14 };
+
+        // Check if the given Suit and Rank are valid
+        if(!ranks.hasOwnProperty(rank))
+            throw `Error: ${rank} is not a proper rank for a card`;
+        if(!suits.hasOwnProperty(suit))
+            throw `Error: ${suit} is not a proper suit for a card`;
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    isLessThan(card) {
+        let suits = { Clubs: 0, Diamonds: 1, Hearts: 2, Spades: 3 };
+        let ranks = { Two: 2, Three: 3, Four: 4, Five: 5, Six: 6, Seven: 7, Eight: 8, 
+                      Nine: 9, Ten: 10, Jack: 11, Queen: 12, King: 13, Ace: 14 };
+
+        if(!(card instanceof Card)) {
+            throw `Error: ${card} is not a card.`;
+        }
+
+        // First check if this card's rank is lower than the given cards rank or if they're the same, then check the suit value
+        if(ranks[this.rank] < ranks[card.rank] ||
+           (this.rank === card.rank && suits[this.suit] < suits[card.suit]))
+            return true;
+        return false;
+    }
+}
+
+var myCard = new Card("Ace", "Spades");
+var myCard2 = new Card("Ten", "Hearts");
+var myCard3 = new Card("Ten", "Diamonds");
+
+console.log(myCard2.isLessThan(myCard3));
---
+++ Deck.js
@@ -0,0 +1,42 @@
+class Deck {
+    constructor() {
+        this.deck = [];
+        let suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+        let ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'];
+
+        // Create standard deck of 52 cards
+        for(let i = 0; i < 4; ++i)
+            for(let j = 0; j < 13; ++j)
+                this.deck.push(new Card(ranks[j], suits[i]));
+    }
+
+    // Shuffle the deck
+    shuffle() {
+        // Use fisher yates algorithm
+        for(let i = this.deck.length-1; i > 0; --i) {
+            const cardSelection = Math.floor(Math.random()*i) + 1;
+            if(cardSelection !== i)
+                this._swapCards(cardSelection, i);
+        }
+    }
+
+    // Draw the last card in the deck
+    draw() {
+        if(this.deck.length === 0)
+            throw 'Error: There are no cards in this deck';
+        return this.deck.splice(-1, 1);
+    }
+
+    // Swap cards given their indexes in the deck
+    _swapCards(i, j) {
+        const temp = this.deck[i];
+        this.deck[i] = this.deck[j];
+        this.deck[j] = temp;
+    }
+}
+
+var myDeck = new Deck();
+myDeck.shuffle();
+console.log(myDeck.deck[51]);
+console.log(myDeck.draw());
+console.log(myDeck.deck);
